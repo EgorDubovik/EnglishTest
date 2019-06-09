145a6ddbac2644a785918e0b073017fce0ba3ae8
EgorDubovik <posik.web.m@gmail.com>
2019-06-08 21:03:37 -0400

    add verbs

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,52 @@
 
 	Test = function(){
 		var _test = {};
-	}
+		var _test_option_1 = {};
+		var verbs = [
+			{
+				rus : "Быть",
+				v1 : "be",
+				v2 : "was, were",
+				v3 : "been"
+			},
+			{
+				rus : "Становиться",
+				v1 : "become",
+				v2 : "became",
+				v3 : "become"
+			},
+			{
+				rus : "Начинать",
+				v1 : "begin",
+				v2 : "began",
+				v3 : "begun"
+			},
+		];
+		_test = {
+			init : function(){
+				
+			}
+		}
+
+		_test_option_1 = {
+			init : function(){
+
+			},
+
+			createTemplate : function(){
+				
+			}
+
+		}
+
+		return {
+			init:_test.init
+		}
+	};
 
 	if (typeof define === "function") {
-		define([], function () { return new Ramka(); });
-	} else if (typeof global.ramka === "undefined") {
-		global.ramka = new Ramka();
+		define([], function () { return new Test(); });
+	} else if (typeof global.test === "undefined") {
+		global.test = new Test();
 	}
 }(this));
